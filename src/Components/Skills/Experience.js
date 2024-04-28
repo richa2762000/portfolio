8b00a8cd64dca8f2a9312d35265781459cb6be6a
Richa Chachan <70852782+richa2762000@users.noreply.github.com>
2024-04-28 20:05:17 +0530

    Update Experience.js
    data add

--- src/Components/Skills/Experience.js
+++ src/Components/Skills/Experience.js
@@ -4,31 +4,43 @@ const Experience = () => {
     <>
       <h1 className="text-center text-3xl font-bold mb-6 ">Work Experience</h1>
       <div className="m-auto w-full sm:w-3/4 md:w-1/2 lg:w-1/2 xl:w-1/2  p-6 md:p-8 border rounded-md shadow-md bg-gradient-to-r from-gray-800 to-gray-900 hover:shadow-lg transform hover:scale-105 transition duration-300 ease-in-out">
-        <p className="font-bold text-xl mb-2 text-white">Associate Software Engineer</p>
-        <p className="text-gray-300 mb-2">March 7, 2022 - Present</p>
-        <p className="text-gray-300 mb-4">Decimal Technology Private Limited - Gurgaon</p>
+        <p className="font-bold text-xl mb-2 text-white">
+          Associate Software Engineer
+        </p>
+        <p className="text-gray-300 mb-2">March, 2022 - Present</p>
+        <p className="text-gray-300 mb-4">
+          Decimal Technology Private Limited - Gurgaon
+        </p>
 
         <h2 className="text-xl font-bold mb-2 text-white">Responsibilities</h2>
         <ul className="list-disc pl-6 text-gray-300">
           <li>
-            Worked on the TATAAIA project at PWC GLOBAL, developing features
-            to ease policy buying and building key components in the frontend.
+            Worked with PWC Global on the TATAAIA project, where I designed
+            certain features to make buying policies easier.Constructing
+            essential front-end elements and evaluating the policies.
           </li>
           <li>
-            Developed a responsive website using React.js and integrated it
-            with external APIs to provide dynamic content.
+            Developed a responsive website using React.js and integrated it with
+            external APIs to provide dynamic content.
           </li>
           <li>
             Collaborated with the team to design and implement user interfaces
-            using React components for efficient data fetching and display.
+            that adapt seamlessly to various devices and screen sizes.Utilized
+            React components to efficiently fetch and display data from the API,
+            enhancing the user experience.
           </li>
           <li>
-            Implemented web applications using frontend technologies such as
-            HTML5, CSS, JavaScript, React.js, Bootstrap, and Angular.
+            Implement the web application using frontend technologies along with
+            Html5, Css, JavaScript, React.js, Bootstrap, Angular.Involved in
+            developing applications using React.js, Bootstrap like GridGrids,
+            Buttons etc.Incorporated router for application.
           </li>
           <li>
-            Debugged and resolved issues in the codebase to ensure smooth
-            functionality and improved performance.
+            We've developed a tool that empowers users to create websites
+            effortlessly. My responsibilities involve debugging the tool to
+            address any user-reported issues, as well as implementing
+            enhancements and integrating additional features to optimize its
+            functionality.
           </li>
         </ul>
       </div>
