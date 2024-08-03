2b6bc26778c9eb341980ffae9f029e81b370cd47
Richa Chachan <70852782+richa2762000@users.noreply.github.com>
2024-08-03 11:47:58 +0530

    Update Value.js

--- src/Components/About/Value.js
+++ src/Components/About/Value.js
@@ -1,24 +1,33 @@
 export const Value = [
-    {
-        id:1,
-      "title": "Movie Recommendation Web App with User Authentication and API Integration.",
-      "order1": "Developed a web application that allows users to sign up, log in, and explore movie recommendations.",
-      "order2": "Integrated with a movie API to fetch data and provide a personalized user experience.",
-      "order3": "Technologies and frameworks used: React.js, HTML, CSS, JavaScript, Bootstrap.",
-    },
-    {
-        id:2,
-      "title": "Responsive Website Development with React.js and API Integration",
-      "order1": "Designed and developed a responsive website using React.js, ensuring optimal user experiences across various devices and screen sizes.Integrated external APIs to fetch real-time data and provide dynamic content to users.",
-      "order2": "Implemented React components and state management to efficiently display and update information retrieved from the API.",
-      "order3": "Utilized modern web development best practices, including responsive design principles and optimization techniques, for improved performance and user engagement.",
-    },
-    {
-      id:3,
-      "title":"Portfolio Website Using React JS ",
-      "order1":"This project appears to be a personal portfolio website showcasing the skills, experience, and projects of a web developer ",
-      "order2":"Showcase Richa Chachan's skills, experience, and personal projects.Provide a visually appealing and responsive design.Enable users to contact Richa through a form. Demonstrate proficiency in modern web development technologies.",
-      "order3":"Languages: HTML5, CSS, JavaScript (React.js). Frameworks: React.js for building dynamic user interfaces.Styling: Tailwind CSS is used for styling components. Icons: FontAwesome icons for social media links.Routing: React Router for navigation."
-    }
-  ];
-  
+  {
+    id: 1,
+    title:
+      "Netflix Project with User Authentication, Movie Management, and GPT-Enhanced Search",
+    order1:
+      "Implemented functionality to redirect users based on their login status, ensuring they are directed to the appropriate page.",
+    order2:
+      "Managed movie data with Redux and fetched movie information from an API.",
+    order3:
+      "Integrated a GPT-powered search feature to filter movies based on user queries, providing a dynamic and interactive experience.",
+  },
+  {
+    id: 2,
+    title: "Responsive Website Development with React.js and API Integration.",
+    order1:
+      "Designed and developed a responsive website using React.js, ensuring optimal user experiences across various devices and screen sizes.",
+    order2:
+      "Implemented React components and state management to efficiently display and update information retrieved from the API.",
+    order3:
+      "Utilized modern web development best practices, including responsive design principles and optimization techniques, for improved performance and user engagement.",
+  },
+  {
+    id: 3,
+    title: "Portfolio Website Using React JS ",
+    order1:
+      "This project appears to be a personal portfolio website showcasing the skills, experience, and projects of a web developer ",
+    order2:
+      "Showcase my skills, experience, and personal projects.Provide a visually appealing and responsive design.Enable users to contact Richa through a form. Demonstrate proficiency in modern web development technologies.",
+    order3:
+      "Provide a visually appealing and responsive design. Enable users to contact with me through a form. Demonstrate proficiency in modern web development technologies.",
+  },
+];
